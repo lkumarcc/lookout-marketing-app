80855ef1dd4ff470e6c5337fdc30b11ec1022a1c
lkumarcc <lkumarcc@gmail.com>
2022-06-13 16:18:49 -0400

    adding basic navbar stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+    </div>
+  );
+}
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +23,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+      </header> */}
 
 export default App;
